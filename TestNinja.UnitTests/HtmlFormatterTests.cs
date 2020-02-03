ede163e85851eeaaa29d9a36808ffcd02f030e3e
Fablo Alencar <fablo.alencar@hotmail.com>
2020-02-03 16:15:22 +0100

    C03: 29. Testing Strings

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,25 @@
+﻿using TestNinja.Fundamentals;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatasBold_WhenCalled_ShouldEncloseTheStringWithStrongElemnt()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("ABC");
+
+            // Specific
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            // More General
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            Assert.That(result, Does.Contain("abc").IgnoreCase);
+        }
+    }
+}
---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,42 @@
+﻿using TestNinja.Fundamentals;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            _math = new Math();
+        }
+
+        [Test]
+        //[Ignore("Because I wanted to")]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            // Act
+            var result = _math.Add(1, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)] // First is greater
+        [TestCase(1, 2, 2)] // Second is greater
+        [TestCase(1, 1, 1)] // They are equal
+        public void Add_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            // Act
+            var result = _math.Max(a, b);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
