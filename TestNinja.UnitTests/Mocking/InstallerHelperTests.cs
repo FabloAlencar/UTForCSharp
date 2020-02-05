34a05779a99e4d2fd749d39ef99817368df533fc
FabloAlencar <57524853+FabloAlencar@users.noreply.github.com>
2020-02-05 22:04:15 +0100

    C14: 64. Testing InstallerHelper

---
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Net;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _mockFileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //Arrange
+            _mockFileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_mockFileDownloader.Object);
+        }
+
+        /*
+         *
+         * 64. Testing InstallerHelper
+         */
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            // .. continue Arrange
+            _mockFileDownloader.Setup(fd =>
+            fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            // Act
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadCompletes_ReturnTrue()
+        {
+            // Act
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+    }
+}
